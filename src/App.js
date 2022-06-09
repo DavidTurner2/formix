bce9c8b904cc60724c729ae1fe14d69d3891b57e
DavidTurner2 <103603180+DavidTurner2@users.noreply.github.com>
2022-06-09 00:25:17 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import ToDoClass from './ToDoClass';
+import {useFormik} from 'formik';
+function App() {
+
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },  onSubmit: values => {
+      alert("Login Successful");
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.email) errors.email = "field required";
+      if (!values.password) errors.password = "field required";
+      return errors;
+    },
+  });
+
+
+  return (
+    <div>
+       <form onSubmit={formik.handleSubmit}>
+   
+      
+       <label htmlFor="email">Email</label>
+        <input
+          id="emailField"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+  {formik.errors.email ? (
+          <div id="emailError" style={{ color: "red" }}>
+            {formik.errors.email}
+          </div>
+        ) : null}
+
+        <div>Password</div>
+       <input
+         id="pswField"
+         name="password"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.password}
+       />
+       <button type="submit">Submit</button>
+     </form>
+      {/* <ToDoClass /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ToDoClass.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+
+class ToDoClass extends React.Component{
+    render(){
+    
+          
+return 
+    }    
+}
+
+export default ToDoClass;
